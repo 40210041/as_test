4965ec1c517b1113b7f1e69dc91b24458b45557c
40210041 <40210041@live.napier.ac.uk>
2018-04-03 17:32:57 +0100

    updated some files, done putExtra tutorial

---
+++ put_extra_test/app/src/main/java/com/example/mandyyip/put_extra_test/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mandyyip.put_extra_test;
+
+import android.content.Intent;
+import android.graphics.pdf.PdfDocument;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void passData(View view)
+    {
+        //set the extras
+        String data_1 = "This is data 1";
+        String data_2 = "This is data 2";
+
+        //create a new intent
+        Intent go_next = new Intent(this, PageTwo.class);
+
+        //create extras to be used in other activities – can do this another way
+        //go_next.putExtra("first_data", data_1);
+        //go_next.putExtra("second_data", data_2);
+
+        //create newe bundle – same as above but less to do
+        Bundle bundle = new Bundle();
+        bundle.putString("first_data", data_1);
+        bundle.putString("second_data", data_2);
+
+        //gathers all extras and sends them togther
+        go_next.putExtras(bundle);
+
+        //show the next activity
+        startActivity(go_next);
+    }
+}
---
+++ put_extra_test/app/src/main/java/com/example/mandyyip/put_extra_test/PageTwo.java
@@ -0,0 +1,40 @@
+package com.example.mandyyip.put_extra_test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class PageTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_two);
+
+        //get the extras
+        Bundle bundle = getIntent().getExtras();
+
+        //pass data to string vars
+        String data_1 = bundle.getString("first_data");
+        String data_2 = bundle.getString("second_data");
+
+        //assign xml obj for txtview into vars
+        TextView txt_one = (TextView)findViewById(R.id.txt_one);
+        TextView txt_two = (TextView)findViewById(R.id.txt_two);
+
+        //set strings
+        txt_one.setText(data_1);
+        txt_two.setText(data_2);
+    }
+
+    public void go_back(View view)
+    {
+        Intent go_back = new Intent(this, MainActivity.class);
+        startActivity(go_back);
+    }
+
+}
